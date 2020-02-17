6766d3d8ac68e81902994d0bbee2207c25aba366
Rahul Gopinath <rahul@gopinath.org>
2020-02-17 18:09:46 +0100

    substitute limit fuzzer

--- src/fuzz.py
+++ src/fuzz.py
@@ -2,6 +2,7 @@ import sys
 import random
 import json
 import re
+import fuzzingbook.Parser as P
 RE_NONTERMINAL = re.compile(r'(<[^<> ]*>)')
 
 class Fuzzer:
@@ -29,22 +30,73 @@ class Fuzzer:
         fuzzed = [self.fuzz_key(token, path) for token in rule]
         return ''.join(fuzzed)
 
+
+class Fuzzer:
+    def __init__(self, grammar):
+        self.grammar = P.canonical(grammar)
+
+    def fuzz(self, key='<start>', max_num=None, max_depth=None):
+        raise NotImplemented()
+
+class LimitFuzzer(Fuzzer):
+    def symbol_cost(self, grammar, symbol, seen):
+        if symbol in self.key_cost: return self.key_cost[symbol]
+        if symbol in seen:
+            self.key_cost[symbol] = float('inf')
+            return float('inf')
+        v = min((self.expansion_cost(grammar, rule, seen | {symbol})
+                    for rule in grammar.get(symbol, [])), default=0)
+        self.key_cost[symbol] = v
+        return v
+
+    def expansion_cost(self, grammar, tokens, seen):
+        return max((self.symbol_cost(grammar, token, seen)
+                    for token in tokens if token in grammar), default=0) + 1
+
+    def gen_key(self, key, depth, max_depth):
+        if key not in self.grammar: return key
+        if depth > max_depth:
+            clst = sorted([(self.cost[key][str(rule)], rule) for rule in self.grammar[key]])
+            rules = [r for c,r in clst if c == clst[0][0]]
+        else:
+            rules = self.grammar[key]
+        return self.gen_rule(random.choice(rules), depth+1, max_depth)
+
+    def gen_rule(self, rule, depth, max_depth):
+        return ''.join(self.gen_key(token, depth, max_depth) for token in rule)
+
+    def fuzz(self, key='<start>', max_depth=10):
+        return self.gen_key(key=key, depth=0, max_depth=max_depth)
+
+    def __init__(self, grammar):
+        super().__init__(grammar)
+        self.key_cost = {}
+        self.cost = self.compute_cost(P.canonical(grammar))
+
+    def compute_cost(self, grammar):
+        cost = {}
+        for k in grammar:
+            cost[k] = {}
+            for rule in grammar[k]:
+                cost[k][str(rule)] = self.expansion_cost(grammar, rule, set())
+        return cost
+
+
 import subprocess
 errors = []
 def main(args):
     with open(args[0]) as f:
         s = json.load(f)
-        f = Fuzzer(s)
+        f = LimitFuzzer(s)
     for i in range(100):
         try:
-            path = []
-            v = f.fuzz_key(args[2] if len(args)> 2 else '<START>', path=path)
+            v = f.fuzz(args[2] if len(args)> 2 else '<START>')
             print(repr(v))
             p = subprocess.Popen(args[1], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             data, err = p.communicate(input=v.encode())
             #print(p.returncode)
             if p.returncode != 0:
-                errors.append((v, path))
+                errors.append((v))
         except RecursionError:
             pass
 
@@ -59,7 +111,6 @@ def process_token(i):
         return repr(i)
 for e,p in errors:
     print(repr(e))
-    print(' '.join([process_token(i) for i in p]))
     print()
 
 print(len(errors))
