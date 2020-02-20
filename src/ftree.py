fb961e8fa28b52a81ea3188150442c2d3ba23b98
Rahul Gopinath <rahul@gopinath.org>
2020-02-20 14:11:14 +0100

    ftree

--- src/ftree.py
+++ src/ftree.py
@@ -41,6 +41,52 @@ ASCII_OPTIONS = Options(FORK=u'|',
                         HORIZONTAL=u'-',
                         NEWLINE=u'\n')
 
+class Colors:
+
+    CBLACK  = '\33[30m'
+    CRED    = '\33[31m'
+    CGREEN  = '\33[32m'
+    CYELLOW = '\33[33m'
+    CBLUE   = '\33[34m'
+    CVIOLET = '\33[35m'
+    CBEIGE  = '\33[36m'
+    CWHITE  = '\33[37m'
+
+    CBLACKBG  = '\33[40m'
+    CREDBG    = '\33[41m'
+    CGREENBG  = '\33[42m'
+    CYELLOWBG = '\33[43m'
+    CBLUEBG   = '\33[44m'
+    CVIOLETBG = '\33[45m'
+    CBEIGEBG  = '\33[46m'
+    CWHITEBG  = '\33[47m'
+
+    CGREY    = '\33[90m'
+    CRED2    = '\33[91m'
+    CGREEN2  = '\33[92m'
+    CYELLOW2 = '\33[93m'
+    CBLUE2   = '\33[94m'
+    CVIOLET2 = '\33[95m'
+    CBEIGE2  = '\33[96m'
+    CWHITE2  = '\33[97m'
+
+    CGREYBG    = '\33[100m'
+    CREDBG2    = '\33[101m'
+    CGREENBG2  = '\33[102m'
+    CYELLOWBG2 = '\33[103m'
+    CBLUEBG2   = '\33[104m'
+    CVIOLETBG2 = '\33[105m'
+    CBEIGEBG2  = '\33[106m'
+    CWHITEBG2  = '\33[107m'
+
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    ENDC = '\033[0m'
 
 def _format_newlines(prefix, formatted_node, options):
     """
@@ -109,7 +155,13 @@ def format_ascii_tree(tree, format_node, get_children):
 
 
 def print_tree(*args, **kwargs):
-    print(format_tree(*args, **kwargs))
+    print(format_ascii_tree(*args, **kwargs))
+
+def hilight_leaf(x):
+    if x[1]:
+        return repr(x[0].encode())
+    else:
+        return Colors.CRED + repr(x[0].encode('utf-8')) + Colors.ENDC
 
 if __name__ == '__main__':
     import sys
@@ -121,4 +173,4 @@ if __name__ == '__main__':
     if len(sys.argv) > 2:
         trees = [int(v) for v in sys.argv[2:]]
     for tree in trees:
-        print_tree(jsont[tree]['tree'], format_node=lambda x: repr(x[0]), get_children=lambda x: x[1])
+        print_tree(jsont[tree]['tree'], format_node=hilight_leaf, get_children=lambda x: x[1])
